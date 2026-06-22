f2daad3ff81f933c3cd983cfd99957e713e4eeca
Sho <293084588+sho-ce-engineer@users.noreply.github.com>
2026-06-22 20:48:03 +0900

    feat:idによる機器の絞り込みAPIの作成

--- packages/api/src/routes/equipments.ts
+++ packages/api/src/routes/equipments.ts
@@ -1,6 +1,7 @@
 import { Hono } from "hono";
 import { db } from "@/db";
 import { equipmentLedger } from "@/db/schema";
+import { eq } from "drizzle-orm";
 
 const app = new Hono();
 
@@ -8,6 +9,16 @@ const app = new Hono();
 app.get("/equipments", async (c) => {
 	const Items = await db.select().from(equipmentLedger);
 	return c.json(Items);
+});
+
+//登録機器の1件取得
+app.get("/equipments/:id", async (c) => {
+	const uniqueId = Number(c.req.param("id"));
+	const targetItem = await db
+		.select()
+		.from(equipmentLedger)
+		.where(eq(equipmentLedger.id, uniqueId));
+	return c.json(targetItem);
 });
 
 export default app;
