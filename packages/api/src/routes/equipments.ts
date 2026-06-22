b6a57b5a4dd8ee86237bd197ca68b09a252239e3
Sho <293084588+sho-ce-engineer@users.noreply.github.com>
2026-06-22 21:38:16 +0900

    feat: equipmentの新規追加APIを実装

--- packages/api/src/routes/equipments.ts
+++ packages/api/src/routes/equipments.ts
@@ -2,6 +2,7 @@ import { Hono } from "hono";
 import { db } from "@/db";
 import { equipmentLedger } from "@/db/schema";
 import { eq } from "drizzle-orm";
+import { createEquipmentSchema } from "@/schemas/equipments";
 
 const app = new Hono();
 
@@ -19,6 +20,22 @@ app.get("/equipments/:id", async (c) => {
 		.from(equipmentLedger)
 		.where(eq(equipmentLedger.id, uniqueId));
 	return c.json(targetItem);
+});
+
+//機器の登録
+app.post("/equipments", async (c) => {
+	const body = await c.req.json();
+	const parceResult = createEquipmentSchema.safeParse(body);
+	if (!parceResult.success) {
+		console.error(`${parceResult.error}`);
+		return c.text("[POST /equipments]Bad Request", 400);
+	} else {
+		const result = await db
+			.insert(equipmentLedger)
+			.values(parceResult.data)
+			.returning();
+		return c.json(result);
+	}
 });
 
 export default app;
