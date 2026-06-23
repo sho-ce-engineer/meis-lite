a2de0f87b52e6b8d8d79169f7b7a8a8c634e1768
Sho <293084588+sho-ce-engineer@users.noreply.github.com>
2026-06-23 20:56:54 +0900

    feat(api): モノレポでのポート競合を避けるため、portを環境変数で設定

--- packages/api/src/index.ts
+++ packages/api/src/index.ts
@@ -4,4 +4,7 @@ import equipments from "@/routes/equipments";
 const app = new Hono();
 app.route("/", equipments); //登録機器の全件取得
 
-export default app;
+export default {
+	port: process.env.PORT ? Number(process.env.PORT) : 3001,
+	fetch: app.fetch,
+};
